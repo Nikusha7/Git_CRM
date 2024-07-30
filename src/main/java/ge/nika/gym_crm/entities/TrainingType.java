310122827a1bc5d3e64319b806a406929eb2bd0a
Nikusha <nikushajv@gmail.com>
2024-07-30 22:41:57 +0400

    updated strategy to GenerationType.IDENTITY

--- src/main/java/ge/nika/gym_crm/entities/TrainingType.java
+++ src/main/java/ge/nika/gym_crm/entities/TrainingType.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "training_type")
 public class TrainingType {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Enumerated(EnumType.STRING)
