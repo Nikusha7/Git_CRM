5021c29b3b2547aca677fe06ef425346eebeddbb
Nikusha <nikushajv@gmail.com>
2024-07-31 01:57:48 +0400

    switched in one case entity by its dto

--- src/main/java/ge/nika/gym_crm/services/TraineeService.java
+++ src/main/java/ge/nika/gym_crm/services/TraineeService.java
@@ -1,12 +1,14 @@
 package ge.nika.gym_crm.services;
 
+import ge.nika.gym_crm.DTO.TraineeDTO;
 import ge.nika.gym_crm.entities.Trainee;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface TraineeService {
-    void create(Trainee trainee);
-    void update(Integer userId, Trainee newTrainee);
-    void delete(Integer userId);
-    Trainee select(Integer userId);
+    Trainee create(TraineeDTO traineeDTO);
+    Trainee select(String userName);
+    Trainee update(Integer id, Trainee newTrainee);
+    void delete(String username);
 }
--- src/main/java/ge/nika/gym_crm/services/TrainerService.java
+++ src/main/java/ge/nika/gym_crm/services/TrainerService.java
@@ -1,11 +1,12 @@
 package ge.nika.gym_crm.services;
 
+import ge.nika.gym_crm.DTO.TrainerDTO;
 import ge.nika.gym_crm.entities.Trainer;
 
 public interface TrainerService {
-    void create(Trainer trainer);
-
+    Trainer create(TrainerDTO trainerDTO);
+    Trainer select(Integer userId);
     void update(Integer userId, Trainer newTrainer);
 
-    Trainer select(Integer userId);
+
 }
