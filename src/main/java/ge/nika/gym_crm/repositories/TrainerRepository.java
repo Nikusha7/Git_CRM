07c2e0091b406d7c2d6f07b79c86fc1dd9654e57
Nikusha <nikushajv@gmail.com>
2024-07-31 02:10:09 +0400

    added custom methods

--- src/main/java/ge/nika/gym_crm/repositories/TrainerRepository.java
+++ src/main/java/ge/nika/gym_crm/repositories/TrainerRepository.java
@@ -1,8 +1,13 @@
 package ge.nika.gym_crm.repositories;
 
+import ge.nika.gym_crm.entities.Trainee;
 import ge.nika.gym_crm.entities.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TrainerRepository
         extends JpaRepository<Trainer, Integer> {
+    Optional<Trainer> findByUser_UserName(String userName);
+    Optional<Trainer> findByUserId(Integer userId);
 }
