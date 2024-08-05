e6bb008be680814ba12d79ec4f545228f2c2d43e
Nikusha <nikushajv@gmail.com>
2024-08-05 11:13:07 +0400

    added training type repo for training type configuration to initialize data once, at app start up.

---
+++ src/main/java/ge/nika/gym_crm/repositories/TrainingTypeRepository.java
@@ -0,0 +1,9 @@
+package ge.nika.gym_crm.repositories;
+
+import ge.nika.gym_crm.entities.TrainingType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainingTypeRepository
+        extends JpaRepository<TrainingType, Integer> {
+
+}
